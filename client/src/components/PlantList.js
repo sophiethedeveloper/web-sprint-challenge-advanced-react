bd8b46a8e084e59f06e10e4ff04780793f84a0f7
sofia Luna <Sofia21luna@gmail.com>
2020-10-02 09:28:06 -0600

    displaying plants

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -1,12 +1,32 @@
 import React, { Component } from "react";
-import axios from "axios";
+// import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
+  state = {
+    plants: [],
+  }
+
+  componentDidMount() {
+    this.fetchPlants(this.state.plants);
+  }
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+
+  fetchPlants = () => {
+    fetch(`http://localhost:3333/plants`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("data", data);
+      this.setState({
+        ...this.state,
+        plants: data.plantsData
+      })
+    })
+    .catch((err) => console.log("error: ", err));
+  }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
